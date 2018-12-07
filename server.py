83bcd1658770c1ccba032ea56f45627304476f5d
figor <fedorovigor1155@gmail.com>
2018-12-07 08:09:12 +0300

    serv up

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-import videostream.stream as stream
-from config import Config
-
 HOST_NAME = 'localhost'
 PORT_NUMBER = 9000
 
@@ -31,7 +28,6 @@ class MyHandler(BaseHTTPRequestHandler):
         content_len = int(self.headers.getheader('content-length', 0))
         post_body = self.rfile.read(content_len)
         print(post_body)
-        main_loop(Config(post_body))
         body = self.rfile.read(int(self.headers.getheader('Content-Length')))
         print (body)
         self.send_response(200)
@@ -39,6 +35,9 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Length", "2")
         self.end_headers()
         self.wfile.write("OK")
+        handle = open("m.json", "w")
+        handle.write(post_body)
+        handle.close()
 
     def handle_http(self, status_code, path):
         self.send_response(status_code)
@@ -50,6 +49,9 @@ class MyHandler(BaseHTTPRequestHandler):
         <p>You accessed path: {}</p>
         </body></html>
         '''.format(path)
+        handle = open("m.json", "w")
+        handle.write("OOOK!")
+        handle.close()
         return bytes(content, 'UTF-8')
 
     def respond(self, opts):
@@ -57,20 +59,13 @@ class MyHandler(BaseHTTPRequestHandler):
         self.wfile.write(response)
 
 
-def main_loop(config):
-    stream.init_stream(config)
-
 if __name__ == '__main__':
     server_class = HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
     print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
-    config = Config("{\"conditions\":[\
-     {\"type\":0,\"operation\":2,\"value\":0,\"event\":0,\"area\":[90,50,183,166]},\
-    {\"type\":1,\"operation\":2,\"value\":0,\"event\":0,\"area\":[90,50,183,166]},\
-    {\"type\":2,\"operation\":2,\"value\":0,\"event\":0,\"area\":[90,50,183,166]},\
-    {\"type\":3,\"operation\":2,\"value\":0,\"event\":0,\"area\":[90,50,183,166]}\
-    ]}")
-    main_loop(config)
+    while(True):
+        if os.path.exists(os.path.join(os.getcwd(), "m.json")):
+            break
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
